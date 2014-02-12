9c873e1c56f1147a8fcdd01b804f744deebb53f6
Huan Du <i@huandu.me>
2014-02-12 22:40:56 +0800

    use `git rev-parse` to get tag canonical revision.

--- lib/puppet/provider/vcsrepo/git.rb
+++ lib/puppet/provider/vcsrepo/git.rb
@@ -52,7 +52,7 @@ Puppet::Type.type(:vcsrepo).provide(:git, :parent => Puppet::Provider::Vcsrepo) 
     return current unless @resource.value(:revision)
 
     if tag_revision?(@resource.value(:revision))
-      canonical = at_path { git_with_identity('show', @resource.value(:revision)).scan(/^commit (.*)/).to_s }
+      canonical = at_path { git_with_identity('rev-parse', @resource.value(:revision)).chomp }
     else
       # if it's not a tag, look for it as a local ref
       canonical = at_path { git_with_identity('rev-parse', '--revs-only', @resource.value(:revision)).chomp }
