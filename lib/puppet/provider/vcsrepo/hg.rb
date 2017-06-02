d248ada58159a215c6507bcb64a9725c43ff34b9
Frank Van Damme <frank.van.damme@vub.ac.be>
2017-06-02 13:04:09 +0200

    hg provider: remove escaped quotes - authentication fix

--- lib/puppet/provider/vcsrepo/hg.rb
+++ lib/puppet/provider/vcsrepo/hg.rb
@@ -127,10 +127,10 @@ Puppet::Type.type(:vcsrepo).provide(:hg, :parent => Puppet::Provider::Vcsrepo) d
 
     if @resource.value(:basic_auth_username) && @resource.value(:basic_auth_password)
       args += [
-        "--config", "\"auth.x.prefix=#{@resource.value(:source)}\"",
-        "--config", "\"auth.x.username=#{@resource.value(:basic_auth_username)}\"",
-        "--config", "\"auth.x.password=#{@resource.value(:basic_auth_password)}\"",
-        "--config", "\"auth.x.schemes=http https\""
+        "--config", "auth.x.prefix=#{@resource.value(:source)}",
+        "--config", "auth.x.username=#{@resource.value(:basic_auth_username)}",
+        "--config", "auth.x.password=#{@resource.value(:basic_auth_password)}",
+        "--config", "auth.x.schemes=http https"
       ]
     end
 
