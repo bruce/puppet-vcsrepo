29d676a6987b25a7bce06592c63574958b25cb5d
Paula McMaw <paula@puppet.com>
2017-07-03 10:21:40 +0100

    (MODULES-5162) - Removing commented out test
    It is not possible for the test to ever pass due to the logic in the
    create/check_force. Removing this test rather than keeping it in the
    code base commented out.

--- spec/unit/puppet/provider/vcsrepo/git_spec.rb
+++ spec/unit/puppet/provider/vcsrepo/git_spec.rb
@@ -222,9 +222,6 @@ branches
       it "should raise an exception" do
         provider.expects(:path_exists?).returns(true)
         provider.expects(:path_empty?).returns(false)
-        ## this test can never succeed due to logic in
-        ##  create/check_force
-        # provider.expects(:working_copy_exists?).returns(false)
         expect { provider.create }.to raise_error(Puppet::Error)
       end
     end
