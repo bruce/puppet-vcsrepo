07537a534cfd4bf66ad6c4d28ac970ac24f5a53a
Mark Overduin <overduin@gmail.com>
2017-10-30 12:48:11 +0100

    (MODULES-5889) Added trust_server_cert support to Git provider
    Added trust_server_cert support to Git provider
    
    Checking for Git version now, the -c parameter wasn't always available
    
    Moved the trust_server_cert part to git_with_identity, so it works for cloning, fetching, etc.
    
    Updated the README.
    
    Changed vcsrepo_git_ver regex to use %r{...}
    
    Using git('--version') now, removed Git version fact.

--- lib/puppet/provider/vcsrepo/git.rb
+++ lib/puppet/provider/vcsrepo/git.rb
@@ -545,6 +545,13 @@ Puppet::Type.type(:vcsrepo).provide(:git, parent: Puppet::Provider::Vcsrepo) do
 
   # @!visibility private
   def git_with_identity(*args)
+    if @resource.value(:trust_server_cert) == :true
+      git_ver = git('--version').match(%r{[0-9]+\.[0-9]+\.[0-9]+(\.[0-9]+)?})[0]
+      git_ver_err = "Can't set sslVerify to false, the -c parameter is not supported in Git #{git_ver}. Please install Git 1.7.2 or higher."
+      return raise(git_ver_err) unless Gem::Version.new(git_ver) >= Gem::Version.new('1.7.2')
+      args.unshift('-c', 'http.sslVerify=false')
+    end
+
     if @resource.value(:identity)
       Tempfile.open('git-helper', Puppet[:statedir]) do |f|
         f.puts '#!/bin/sh'
