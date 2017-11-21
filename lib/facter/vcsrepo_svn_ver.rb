01871cfe37ad8ae705938b8e004a92dbf5d1df4f
Chris Girard <cgirard@arxan.com>
2017-11-21 00:54:11 -0500

    Add guard for macOS to avoid popup dialog when developer tools aren't installed.

--- lib/facter/vcsrepo_svn_ver.rb
+++ lib/facter/vcsrepo_svn_ver.rb
@@ -1,9 +1,13 @@
 Facter.add(:vcsrepo_svn_ver) do
   setcode do
     begin
-      version = Facter::Core::Execution.execute('svn --version --quiet')
-      if Gem::Version.new(version) > Gem::Version.new('0.0.1')
-        version
+      unless Facter.value(:operatingsystem) == 'Darwin' and not File.directory?(Facter::Core::Execution.execute('xcode-select -p'))
+        version = Facter::Core::Execution.execute('svn --version --quiet')
+        if Gem::Version.new(version) > Gem::Version.new('0.0.1')
+          version
+        else
+          ''
+        end
       else
         ''
       end
