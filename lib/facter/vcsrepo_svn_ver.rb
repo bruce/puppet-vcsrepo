926b312ecb3f8b67941395b3476952666cf97891
Chris Girard <cgirard@arxan.com>
2018-01-05 13:24:47 -0500

    Fix RuboCop violations.

--- lib/facter/vcsrepo_svn_ver.rb
+++ lib/facter/vcsrepo_svn_ver.rb
@@ -1,15 +1,15 @@
 Facter.add(:vcsrepo_svn_ver) do
   setcode do
     begin
-      unless Facter.value(:operatingsystem) == 'Darwin' and not File.directory?(Facter::Core::Execution.execute('xcode-select -p'))
+      if Facter.value(:operatingsystem) == 'Darwin' && !File.directory?(Facter::Core::Execution.execute('xcode-select -p'))
+        ''
+      else
         version = Facter::Core::Execution.execute('svn --version --quiet')
         if Gem::Version.new(version) > Gem::Version.new('0.0.1')
           version
         else
           ''
         end
-      else
-        ''
       end
     rescue StandardError
       ''
